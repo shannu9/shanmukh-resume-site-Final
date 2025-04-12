79794433bcde76abda202d0ee38b14bbe33dfbc0
shanmukh  sri surya gopi <gopishanmukha@gmail.com>
2025-04-12 15:45:53 -0400

    Update

--- src/pages/admin/CertificationsManager.jsx
+++ src/pages/admin/CertificationsManager.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { db, storage } from "../firebase";
+import { db, storage } from "../../firebase";
 import { collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
