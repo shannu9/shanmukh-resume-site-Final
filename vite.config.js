519c7c2daacbf9082e8091112116101824fb1520
shanmukh  sri surya gopi <gopishanmukha@gmail.com>
2025-04-20 02:12:27 -0400

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: ".",
-  publicDir: "public",
   plugins: [react()],
   build: {
     outDir: "dist",
