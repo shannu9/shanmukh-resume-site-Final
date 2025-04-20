607bf549101c6d3a2b9451f5db3632c60680404c
shanmukh  sri surya gopi <gopishanmukha@gmail.com>
2025-04-20 02:37:13 -0400

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import { AuthProvider } from "./contexts/AuthContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
       <App />
-    </AuthProvider>
   </React.StrictMode>
 );
